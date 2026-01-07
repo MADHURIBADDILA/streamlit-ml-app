0f644d94f1452bf549731d2beb92cec00660dfcf
MADHURI BADDILA <madhurimadhuri07466@gmail.com>
2026-01-07 09:47:23 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+#import library
+import streamlit as st
+
+#title
+st.title("Hello,👋 World!🌍 Welcome to AICW program!👩‍💻.")
+
+#header
+st.header("Header in streamlit.")
+
+#subheader
+st.subheader("Subheader in streamlit.")
